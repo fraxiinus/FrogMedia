985d137f9580f6948ab56216268ca8472ada34da
A Lee <andrew1421lee@gmail.com>
2020-07-11 11:36:06 -0400

    spawn posts

--- Assets/Scripts/PostGenerator.cs
+++ Assets/Scripts/PostGenerator.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public class PostGenerator : MonoBehaviour
 {
     public GameObject PageContainer;
+    
+    public GameObject PostPrefab;
 
-    public List<GameObject> tabs;
+    private List<GameObject> tabs;
 
     private TextParser loader;
 
@@ -25,6 +28,33 @@ public class PostGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            SpawnPost(0, "frog name", "I spawned this post through a script!");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            SpawnPost(1, "frog name", "I spawned this post through a script!");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            SpawnPost(2, "frog name", "I spawned this post through a script!");
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            SpawnPost(3, "frog name", "I spawned this post through a script!");
+        }
+    }
+
+    void SpawnPost(int tabIndex, string name, string content)
+    {
+        var newPost = Instantiate(PostPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        newPost.FindChildObject("Content").GetComponent<Text>().text = content.ToUpper();
+        newPost.FindChildObject("Username").GetComponent<Text>().text = name.ToUpper();
+
+        newPost.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 130);
+        newPost.transform.SetParent(tabs[tabIndex].transform);
+        newPost.transform.localScale = new Vector3(1, 1, 1);
     }
 }
