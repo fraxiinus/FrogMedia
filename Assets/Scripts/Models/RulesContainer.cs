d38961bbea06d7c7bf5f465eaf7924a5370aab0f
P. Maino <pcmaino@gmail.com>
2020-07-12 12:17:05 -0400

    Added random fake post generations.

---
+++ Assets/Scripts/Models/RulesContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class RulesContainer
+{
+    public RulesContainer(List<string> r) {
+        rules = r;
+    }
+    public List<string> rules { get; set; }
+}
